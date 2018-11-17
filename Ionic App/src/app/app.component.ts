c78711463925358bc9ca1f65af3507d9e1860382
Ashwini <thaash16@student.westerdals.no>
2018-11-17 15:54:40 +0100

    Profile
    Profiles added buttons

--- Ionic App/src/app/app.component.ts
+++ Ionic App/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component,ViewChild } from '@angular/core';
-import {NavController, Platform} from 'ionic-angular';
+import { Component, } from '@angular/core';
+import { Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {TabsPage} from "../pages/tabs/tabs";
@@ -18,8 +18,5 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
-    onLoad(page:any){
-
-    }
   }
 }
