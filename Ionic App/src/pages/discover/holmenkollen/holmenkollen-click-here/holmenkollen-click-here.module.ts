276b0b6195058beeaff39170feadd0666630a7b4
Ashwini <thaash16@student.westerdals.no>
2018-11-18 20:15:35 +0100

    Martins del

---
+++ Ionic App/src/pages/discover/holmenkollen/holmenkollen-click-here/holmenkollen-click-here.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HolmenkollenClickHerePage } from './holmenkollen-click-here';
+
+@NgModule({
+  declarations: [
+    HolmenkollenClickHerePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HolmenkollenClickHerePage),
+  ],
+})
+export class HolmenkollenClickHerePageModule {}
